c083bc4fceb931ea8ce9fb2903c1d05c3f8f5f50
unknown <hninhnin0929>
2024-05-25 11:50:56 +0900

    getting start with counter jotai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-
+"use client"
 import { atom, useAtom } from 'jotai';
+import { countAtom } from './store';
 
 export default function Home() {
+  const [count, setCounter] = useAtom(countAtom);
+  const onClick = () => setCounter(prev => prev +1 );
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Hello
+      <div className="z-10 w-full max-w-5xl items-center flex flex-col font-mono text-sm lg:flex">
+        <h1>{count}</h1>
+        <button onClick={onClick} className='border-t-indigo-500 bg-blue-950 text-white'>
+          Click
+        </button>
       </div>
     </main>
   );
