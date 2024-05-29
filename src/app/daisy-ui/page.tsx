b7068ae9e2b6974449a66271b6f9e833df915b5d
hnin <hnin0929@ecplaza.net>
2024-05-29 17:56:24 +0900

    daisy ui testing

--- src/app/daisy-ui/page.tsx
+++ src/app/daisy-ui/page.tsx
@@ -1,9 +1,294 @@
+"use client";
+
 export default function DaisyUITest() {
+  const openModal = () => {
+    const modal = document.getElementById(
+      "my_modal_5"
+    ) as HTMLDialogElement | null;
+    if (modal) {
+      modal.showModal();
+    }
+  };
   return (
     <div>
-      <button className="btn btn-primary">One</button>
-      <button className="btn btn-secondary">Two</button>
-      <button className="btn btn-accent btn-outline">Three</button>
+      <div>
+        <button className="btn btn-primary">One</button>
+        <button className="btn btn-secondary">Two</button>
+        <button className="btn btn-accent btn-outline">Three</button>
+      </div>
+
+      <div>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+      </div>
+
+      <div>
+        <button className="btn glass">Glass button</button>
+      </div>
+
+      <div>
+        <button className="btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+          Button
+        </button>
+        <button className="btn">
+          <span className="loading loading-spinner"></span>
+          loading
+        </button>
+      </div>
+
+      <div className="dropdown dropdown-hover">
+        <div tabIndex={0} role="button" className="btn m-1">
+          Hover
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        {/* daisy modal */}
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <button className="btn" onClick={openModal}>
+          open modal
+        </button>
+        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">
+              Press ESC key or click the button below to close
+            </p>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
+
+      <div>
+        <label className="btn btn-circle swap swap-rotate">
+          {/* this hidden checkbox controls the state */}
+          <input type="checkbox" />
+
+          {/* hamburger icon */}
+          <svg
+            className="swap-off fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+          </svg>
+
+          {/* close icon */}
+          <svg
+            className="swap-on fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+          </svg>
+        </label>
+      </div>
+
+      <div>
+        <label className="swap text-6xl">
+          <div className="swap-on">🥵</div>
+          <div className="swap-off">🥶</div>
+        </label>
+        <label className="swap swap-active text-6xl">
+          <div className="swap-on">🥳</div>
+          <div className="swap-off">😭</div>
+        </label>
+      </div>
+
+      <div className="avatar-group -space-x-6 rtl:space-x-reverse">
+        <div className="avatar">
+          <div className="w-12">
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="avatar">
+          <div className="w-12">
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="avatar">
+          <div className="w-12">
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="avatar placeholder">
+          <div className="w-12 bg-neutral text-neutral-content">
+            <span>+99</span>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Shoes!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Fashion</div>
+              <div className="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="carousel carousel-center rounded-box">
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+              alt="Pizza"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+              alt="Pizza"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="chat chat-start">
+          <div className="chat-header">
+            Obi-Wan Kenobi
+            <time className="text-xs opacity-50">2 hours ago</time>
+          </div>
+          <div className="chat-bubble">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Seen</div>
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-header">
+            Obi-Wan Kenobi
+            <time className="text-xs opacity-50">2 hour ago</time>
+          </div>
+          <div className="chat-bubble">I loved you.</div>
+          <div className="chat-footer opacity-50">Delivered</div>
+        </div>
+      </div>
+
+      <div>
+        <span className="loading loading-bars loading-xs"></span>
+        <span className="loading loading-bars loading-sm"></span>
+        <span className="loading loading-bars loading-md"></span>
+        <span className="loading loading-bars loading-lg"></span>
+      </div>
+
+      <div>
+        <div className="flex flex-col gap-4 w-52">
+          <div className="flex gap-4 items-center">
+            <div className="skeleton w-16 h-16 rounded-full shrink-0"></div>
+            <div className="flex flex-col gap-4">
+              <div className="skeleton h-4 w-20"></div>
+              <div className="skeleton h-4 w-28"></div>
+            </div>
+          </div>
+          <div className="skeleton h-32 w-full"></div>
+        </div>
+      </div>
+      
     </div>
   );
 }
